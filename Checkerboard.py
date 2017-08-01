a0c2724132fd8f1befe7fbef68a019839cbf565b
JamesORourke <geekjamesorourkejr@icloud.com>
2017-07-31 18:04:24 -0700

    Assignment Done

---
+++ Checkerboard.py
@@ -0,0 +1,9 @@
+def printCheckerboard() :
+    for i in range(1,9):
+        if ((i % 2) == 1):
+            print ("* * * * ")
+        else:
+            print (" * * * *")
+    return;
+
+printCheckerboard()
